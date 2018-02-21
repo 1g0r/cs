add2310258df6cc42e0d1938f761202a45cb4b78
1g0r <logovskoy@gmail.com>
2018-02-21 18:36:08 +0300

    Create IResourcePoolFactory.cs

---
+++ ObjectPool/IResourcePoolFactory.cs
@@ -0,0 +1,8 @@
+namespace ObjectPool
+{
+	public interface IResourcePoolFactory
+	{
+		IResourcePool<T> CreatePoolWithLoader<T>(IResourcesLoader<T> loader) where T: PoolItemBase;
+		IResourcePool<T> CreatePool<T>(int minimumSize, int maximumSize, Func<T> factoryMethod) where T: PoolItemBase;
+	}
+}
