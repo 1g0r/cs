b33718b92f2afe7c888ffe00abb81bebe1d1d1be
1g0r <logovskoy@gmail.com>
2021-01-04 00:01:10 +0300

    Update Nested.cs

--- nested/Nested.cs
+++ nested/Nested.cs
@@ -2,17 +2,28 @@ using System;
 
 namespace LoL
 {
-    public static class Nested
+     public static class Nested
     {
-        public static Func<Func<T1, T>, T> Start<T1, T>(this Func<Func<T1, T>, T> prev){
-            return (Func<T1, T> callback) => prev(callback);
+        public delegate TResult Wrap<T, TResult>(Func<T, TResult> callback);
+        public delegate TResult Wrap<T1, T2, TResult>(T1 x, Func<T2, TResult> callback);
+        
+        public static Wrap<T, TResult> Start<T, TResult>(this Wrap<T, TResult> prev)
+        {
+            return new Wrap<T, TResult>(callback => prev(callback));
         }
         
-        public static Func<Func<T2, T>, T> Next<T1, T2, T>(this Func<Func<T1, T>, T> prev, Func<T1, Func<T2, T>, T> next){
-            return (Func<T2, T> callback) => prev((T1 x) => next(x, callback));
+        public static Wrap<T2, TResult> Start<T1, T2, TResult>(T1 x, Wrap<T1, T2, TResult> prev)
+        {
+            return new Wrap<T2, TResult>(callback => prev(x, callback));
         }
         
-        public static Func<T> Last<T1, T>(this Func<Func<T1, T>, T> prev, Func<T1, T> callback){
+        public static Wrap<T2, TResult> Next<T1, T2, TResult>(this Wrap<T1, TResult> prev, Wrap<T1, T2, TResult> next)
+        {
+            return new Wrap<T2, TResult>(callback => prev(x => next(x, callback)));
+        }
+        
+        public static Func<TResult> Last<T, TResult>(this Wrap<T, TResult> prev, Func<T, TResult> callback)
+        {
             return () => prev(callback);
         }
     }
@@ -25,7 +36,8 @@ namespace LoL
             return callback(flag);
         }
         
-        private static string PassBool(bool flag, Func<bool, string> callback){
+        private static string PassBool(bool flag, Func<bool, string> callback)
+        {
             return callback(flag);
         }
 
@@ -37,19 +49,20 @@ namespace LoL
             return callback(666);
         }
         
-        private static string GetString(int value){
+        private static string GetString(int value)
+        {
             return value.ToString() + " Fuck!";
         }
         
         public static void Main(string[] args)
         {
             //var t = GetBool(b => GetInt(b, GetString));
-            var gb = Start<bool, string>(GetBool);            
+            var gb = Nested.Start<bool, string>(GetBool);            
             var gi = gb.Next<bool, int, string>(GetInt);            
             var gs = gi.Last(GetString);
             Console.WriteLine(gs());
             
-            var tt = Start<bool, string>((f) => PassBool(true, f))
+            var tt = Nested.Start<bool, bool, string>(true, PassBool)
                 .Next<bool, int, string>(GetInt)
                 .Last(GetString);
             Console.WriteLine(tt());
