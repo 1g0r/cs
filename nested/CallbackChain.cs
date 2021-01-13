44514c785687a7aba531c2b47e133c2e9659a51f
1g0r <logovskoy@gmail.com>
2021-01-13 08:57:34 +0300

    Update CallbackChain.cs

--- nested/CallbackChain.cs
+++ nested/CallbackChain.cs
@@ -2,58 +2,68 @@ using System;
 
 namespace LoL
 {
-	public static class CallbackChain<TResult>
+    public static class CallbackChain<TResult>
+    {
+        public class NextCall
 	{
-		public static NextCall<T> Run<T>(Func<Func<T, TResult>, TResult> prev)
-		{
-			return new NextCall<T>(callback => prev(callback));
-		}
-		
-		public static NextCall Run<T1, T2>(T1 a, T2 b, Func<T1, T2, Func<TResult>, TResult> prev)
-		{
-			return new NextCall(callback => prev(a, b, callback));
-		}
-		
-		public class NextCall
-		{
-			private readonly Func<Func<TResult>, TResult> _prev;
+	    private readonly Func<Func<TResult>, TResult> _prev;
 			
-			public NextCall(Func<Func<TResult>, TResult> prev)
-			{
-				_prev = prev;
-			}
+	    public NextCall(Func<Func<TResult>, TResult> prev)
+	    {
+		_prev = prev;
+	    }
 
-			public NextCall<T> Then<T>(Func<Func<T, TResult>, TResult> next)
-			{
-				return new NextCall<T>(callback => _prev(() => next(callback)));
-			}
-		}
+	    public NextCall<T> Then<T>(Func<Func<T, TResult>, TResult> next)
+	    {
+		return new NextCall<T>(callback => _prev(() => next(callback)));
+	    }
+        }
 		
-		public class NextCall<T> : NextCall
-		{
-			private readonly Func<Func<T, TResult>, TResult> _prev;
+	public class NextCall<T1>
+	{
+	    private readonly Func<Func<T1, TResult>, TResult> _prev;
 			
-			public NextCall(Func<Func<T, TResult>, TResult> prev) : base(null)
-			{
-				_prev = prev;
-			}
+	    public NextCall(Func<Func<T1, TResult>, TResult> prev)
+	    {
+	        _prev = prev;
+	    }
 			
-			public NextCall<TT> Then<TT>(Func<T, Func<TT, TResult>, TResult> next)
-			{
-				return new NextCall<TT>(callback => _prev(x => next(x, callback)));
-			}
-                        
-                        public NextCall<TT> Then<TT>(Func<T, TT> with)
-                        {
-                                return new NextCall<TT>(callback => _prev(x => callback(with(x)) ));
-                        }	
+	    public NextCall<T2> Then<T2>(Func<T1, Func<T2, TResult>, TResult> next)
+	    {
+		return new NextCall<T2>(callback => _prev(x => next(x, callback)));
+	    }
 
-			public TResult Then(Func<T, TResult> next)
-			{
-				return _prev(next);
-			}
-		}
+            public NextCallWith<T2> With<T2>(Func<T1, T2> factory)
+            {
+                return new NextCallWith<T2>(callback => _prev(x => callback(() => factory(x))));
+            }
+
+	    public TResult Return(Func<T1, TResult> next)
+	    {
+		return _prev(next);
+	    }
 	}
+        
+        public class NextCallWith<T>
+        {
+            private readonly Func<Func<Func<T>, TResult>, TResult> _prev;
+
+            public NextCallWith(Func<Func<Func<T>, TResult>, TResult> prev)
+            {
+                _prev = prev;
+            }
+
+            public NextCall<T> Then(Func<Func<T>, Func<T, TResult>, TResult> next)
+            {
+                return new NextCall<T>(callback => _prev(x => next(x, callback)));
+            }
+        }
+
+        public static NextCall Enter<T1, T2, T3>(Func<T1, T2, T3, Func<TResult>, TResult> prev, T1 a, T2 b, T3 c)
+        {
+            return new NextCall(callback => prev(a, b, c, callback));
+        }
+    }
 	
 	// DUMMY
 	public class SqlReader : IDisposable 
