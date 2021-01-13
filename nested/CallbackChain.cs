ad2867d6562833b192357ef8dceb8e97c1ff6d96
1g0r <logovskoy@gmail.com>
2021-01-13 09:11:30 +0300

    Update CallbackChain.cs

--- nested/CallbackChain.cs
+++ nested/CallbackChain.cs
@@ -65,97 +65,101 @@ namespace LoL
         }
     }
 	
-	// DUMMY
-	public class SqlReader : IDisposable 
+    // DUMMY
+    public class SqlReader : IDisposable 
+    {
+	public void Dispose()
 	{
-		public void Dispose()
-		{
-			Console.WriteLine("Reader disposed!");
-		}
-		public string Read()
-		{
-			return "Hello from reader!!";
-		}
+	    Console.WriteLine("Reader disposed!");
 	}
-	public class SqlConn : IDisposable 
+	public string Read()
 	{
-		public void Dispose()
-		{
-			Console.WriteLine("Connection disposed!");
-		}
-		public void Open(){
-			Console.WriteLine("Connection opened!");
-		}
-		public SqlReader GetReader()
-		{
-			Console.WriteLine("Reader created!");
-			return new SqlReader();
-		}
+	    return "Hello from reader!!";
 	}
+    }
+
+    public class SqlConn : IDisposable 
+    {
+	public void Dispose()
+	{
+	    Console.WriteLine("Connection disposed!");
+	}
+	public void Open()
+        {
+	    Console.WriteLine("Connection opened!");
+	}
+	public SqlReader GetReader()
+	{
+	    Console.WriteLine("Reader created!");
+	    return new SqlReader();
+	}
+    }
 	
     public static class Program
     {
-		private static TResult SecureEval<TResult>(string prevMsg, string errorMsg, Func<TResult> executor)
-		{
-			try
-			{
-				Console.WriteLine(prevMsg);
-				var result = executor();
-				Console.WriteLine("Exit SecureEval");
-				return result;
-			}
-			catch(Exception ex)
-			{
-				Console.WriteLine(errorMsg + ex.ToString());
-				throw;
-			}
-		}
+	private static TResult SecureEval<TResult>(string prevMsg, string errorMsg, Func<TResult> executor)
+	{
+	    try
+	    {
+		Console.WriteLine(prevMsg);
+		var result = executor();
+		Console.WriteLine("Exit SecureEval");
+		return result;
+	    }
+	    catch(Exception ex)
+	    {
+		Console.WriteLine(errorMsg + ex.ToString());
+		throw;
+	    }
+	}
 		
-		private static TResult OpenConnection<TResult>(Func<SqlConn, TResult> cmdExecutor)
-		{
-			using(var conn = new SqlConn())
-			{
-				conn.Open();
-				return cmdExecutor(conn);
-			}
-		}
+	private static TResult OpenConnection<TResult>(Func<SqlConn, TResult> cmdExecutor)
+	{
+	    using(var conn = new SqlConn())
+	    {
+		conn.Open();
+		return cmdExecutor(conn);
+	    }
+	}
 		
-		private static TResult OpenReader<TResult>(SqlConn conn, Func<SqlReader, TResult> resultReader)
-		{
-			using(var reader = conn.GetReader())
-			{
-				return resultReader(reader);
-			}
-		}
+	private static TResult OpenReader<TResult>(Func<DataReader> factory, Func<SqlReader, TResult> resultReader)
+	{
+	    using(var reader = factory())
+	    {
+		return resultReader(reader);
+	    }
+	}
 		
-		private static string Read(SqlReader reader)
-		{
-			return reader.Read();
-		}
+	private static string Read(SqlReader reader)
+	{
+	    return reader.Read();
+	}
 		
-		/* int */
-		private static int ReadInt(SqlReader reader)
-		{
-			return 666;
-		}
+	/* int */
+	private static int ReadInt(SqlReader reader)
+	{
+	    return 666;
+	}
         
         public static void Main(string[] args)
         {
-			var t = CallbackChain<string>
-				.Run("Begin secure", "fooo", SecureEval)
-				.Then<SqlConn>(OpenConnection)
-				.Then<SqlReader>(OpenReader)
-				.Then(Read);
-			Console.WriteLine("Result = {0}, Type = {1}", t, t.GetType());
+	    var t = CallbackChain<string>
+		.Enter("Begin secure", "fooo", SecureEval)
+		.Then<SqlConn>(OpenConnection)
+                .With(conn => conn.GetReader())
+		.Then(OpenReader)
+		.Return(Read);
+	    Console.WriteLine("Result = {0}, Type = {1}", t, t.GetType());
 			
-			Console.WriteLine("");
+	    Console.WriteLine("");
 			
-			var tt = CallbackChain<int>
-				.Run("Begin secure", "fooo", SecureEval)
-				.Then<SqlConn>(OpenConnection)
-				.Then<SqlReader>(OpenReader)
-				.Then(ReadInt);
-			Console.WriteLine("Result = {0}, Type = {1}", tt, tt.GetType());
+	    var tt = CallbackChain<int>
+		.Run("Begin secure", "fooo", SecureEval)
+		.Then<SqlConn>(OpenConnection)
+                .With(c => c.GetReader())
+	        .Then(OpenReader)
+		.Return(ReadInt);
+	    Console.WriteLine("Result = {0}, Type = {1}", tt, tt.GetType());
         }
     }
 }
